687eba7aed02134936b0b6f211e48c745596ae61
PressureDraper <warmarines00@hotmail.com>
2024-12-10 19:05:45 -0600

    add: queue page progress

---
+++ src/helpers/data.ts
@@ -0,0 +1,37 @@
+export const data: { ticketNo: number, desktop: number, agent: string }[] = [
+    {
+        ticketNo: 33,
+        desktop: 3,
+        agent: 'Fernando Herrera'
+    },
+    {
+        ticketNo: 34,
+        desktop: 4,
+        agent: 'Melissa Flores'
+    },
+    {
+        ticketNo: 35,
+        desktop: 5,
+        agent: 'Carlos Castro'
+    },
+    {
+        ticketNo: 36,
+        desktop: 3,
+        agent: 'Fernando Herrera'
+    },
+    {
+        ticketNo: 37,
+        desktop: 3,
+        agent: 'Fernando Herrera'
+    },
+    {
+        ticketNo: 38,
+        desktop: 2,
+        agent: 'Melissa Flores'
+    },
+    {
+        ticketNo: 39,
+        desktop: 5,
+        agent: 'Carlos Castro'
+    },
+];
--- src/pages/Queue.tsx
+++ src/pages/Queue.tsx
@@ -1,6 +1,50 @@
+import { Card, Col, Divider, List, Row, Tag, Typography } from "antd"
+import { data } from "../helpers/data";
 
 export const Queue = () => {
+    const { Title, Text } = Typography;
+
     return (
-        <div>Queue</div>
+        <>
+            <Title level={1}>Serving Customer</Title>
+            <Row>
+                <Col span={12}>
+                    <List
+                        dataSource={data.slice(0, 3)}
+                        renderItem={item => (
+                            <List.Item>
+                                <Card
+                                    style={{ width: 300, marginTop: 15 }}
+                                    actions={[
+                                        <Tag color="volcano"> {item.agent} </Tag>,
+                                        <Tag color="magenta"> {item.desktop} </Tag>
+                                    ]}
+                                >
+                                    <Title>No. {item.ticketNo}</Title>
+                                </Card>
+                            </List.Item>
+                        )}
+                    />
+                </Col>
+                <Col span={12}>
+                    <Divider>History</Divider>
+                    <List 
+                    dataSource={data.slice(3)}
+                    renderItem={item => (
+                        <List.Item>
+                            <List.Item.Meta 
+                                title={`Ticket No. ${item.ticketNo}`}
+                                description={
+                                    <>
+                                        <Text type="secondary">On desktop</Text>
+                                    </>
+                                }
+                            />
+                        </List.Item>
+                    )}
+                    />
+                </Col>
+            </Row>
+        </>
     )
 }
